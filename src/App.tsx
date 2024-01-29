f6037c5d49887045398dd6475eb11272d1de2e29
Yuri <Yuri@Lian.me>
2024-01-29 18:39:23 +0000

    Displayed Genre Images

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ function App() {
   return <Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
- }}>
-   <GridItem area='nav'>
-     <NavBar></NavBar>
-   </GridItem>
-   <Show above="lg">
-     <GridItem area='aside'>
-      <GenreList />
-     </GridItem>
-   </Show>
-   <GridItem area='main'>
-     <GameGrid />
-   </GridItem>
- </Grid>
+  }}
+  templateColumns={{
+    base: '1fr',
+    lg: '200px 1fr'
+  }}>
+    <GridItem area='nav'>
+      <NavBar></NavBar>
+    </GridItem>
+    <Show above="lg">
+      <GridItem area='aside' paddingX={5}>
+        <GenreList />
+      </GridItem>
+    </Show>
+    <GridItem area='main'>
+      <GameGrid />
+    </GridItem>
+  </Grid>
 }
 export default App;
